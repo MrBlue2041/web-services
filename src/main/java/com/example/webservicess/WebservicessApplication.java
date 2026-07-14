0dbc9e9761833dae564e2d6d7c3c7457449750f6
MrBlue <julianostz137@gmail.com>
2026-07-14 16:39:14 -0300

    chore: initial project setup

---
+++ src/main/java/com/example/webservicess/WebservicessApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webservicess/WebservicessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
