ea71ff2950a36fc0db2cca4b1b88637d37352802
MrBlue <julianostz137@gmail.com>
2026-07-14 16:50:31 -0300

    feat: create UserRepository

---
+++ src/main/java/com/example/webservicess/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.webservicess.repository;
+
+import com.example.webservicess.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
