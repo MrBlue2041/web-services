a16ffa5f51aa0c1a947611b2d2b54705b86eaad5
MrBlue <julianostz137@gmail.com>
2026-07-15 15:53:52 -0300

    feat: create UserService and implements UserResource

--- src/main/java/com/example/webservicess/resources/UserResource.java
+++ src/main/java/com/example/webservicess/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.example.webservicess.resources;
 
 import com.example.webservicess.entities.User;
+import com.example.webservicess.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    ResponseEntity<User> findAll(){
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        return ResponseEntity.ok(u1);
+    ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
     }
 }
