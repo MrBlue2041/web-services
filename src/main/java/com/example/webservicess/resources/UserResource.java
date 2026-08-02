80b213266d27e4a2e141d8ac231f96196113f51b
MrBlue <julianostz137@gmail.com>
2026-08-02 14:18:42 -0300

    feat: create new exceptions

--- src/main/java/com/example/webservicess/resources/UserResource.java
+++ src/main/java/com/example/webservicess/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.example.webservicess.resources;
 
 import com.example.webservicess.entities.User;
 import com.example.webservicess.services.UserService;
+import jdk.jfr.StackTrace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,5 +34,18 @@ public class UserResource {
         obj = userService.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok(obj);
+
     }
 }
--- src/main/java/com/example/webservicess/services/UserService.java
+++ src/main/java/com/example/webservicess/services/UserService.java
@@ -2,7 +2,13 @@ package com.example.webservicess.services;
 
 import com.example.webservicess.entities.User;
 import com.example.webservicess.repository.UserRepository;
+import com.example.webservicess.services.exceptions.DatabaseException;
+import com.example.webservicess.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.boot.model.relational.Database;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,10 +25,40 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
         return userRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+
+        try {
+            userRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public User update(Long id, User obj){
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
